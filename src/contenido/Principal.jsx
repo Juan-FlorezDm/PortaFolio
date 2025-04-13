5350d86cff87dfe28aac4c21c4db53714d998a1f
Juan-FlorezDm <jnendez38@gmail.com>
2025-04-13 16:23:28 -0500

    cambio de background

--- src/contenido/Principal.jsx
+++ src/contenido/Principal.jsx
@@ -42,56 +42,59 @@ export default function Principal(){
     return(
         <>
             <div className="informacion">
-                <div className='contenido'>
-                    <img 
-                    src="/personal.jpeg"
-                    alt="foto de perfil" 
-                    style={{borderRadius: "100%", width: '80%'}}
-                    />
+                <div className='contenido'>  
+                    <div className='fondo'></div> 
+                    <div className='conten'>
+                        <img 
+                        src="/personal.jpeg"
+                        alt="foto de perfil" 
+                        style={{borderRadius: "100%", width: '80%'}}
+                        />
 
-                    <nav>
-                        <ul>
-                            <li><a href="https://github.com/Juan-FlorezDm" target='_blank'>Github</a></li>
-                            <li><a href="https://www.linkedin.com/in/juan-diego-florez-mendez-632522234/" target='_blank'>Linkedln</a></li>
-                            <li><a href="https://open.spotify.com/user/31ijr27ek5q36thgywgia2cnmqy4?si=b1299b6eee6b4d4a" target='_blank'>Spotify</a></li>
-                        </ul>
-                    </nav>
-                    <p>
-                    Hola, soy estudiante de ingeniería de sistemas
-                    Me apasiona aprender cosas nuevas, de cualquier tema,
-                    me apasiona conocer personas ampliar mi circulo social.< br />
-                    <br />
-                    Soy entusiasta, y me esfuerzo por aprender tecnologías como
-                    Java, Javascrpit, HTML, CSS, React, Linux, herramientas
-                    de búsqueda de vulnerabilidades, la computación
-                    en la nube como AWS, Oracle cloud
-                    </p>
+                        <nav>
+                            <ul>
+                                <li><a href="https://github.com/Juan-FlorezDm" target='_blank'>Github</a></li>
+                                <li><a href="https://www.linkedin.com/in/juan-diego-florez-mendez-632522234/" target='_blank'>Linkedln</a></li>
+                                <li><a href="https://open.spotify.com/user/31ijr27ek5q36thgywgia2cnmqy4?si=b1299b6eee6b4d4a" target='_blank'>Spotify</a></li>
+                            </ul>
+                        </nav>
+                        <p>
+                        Hola, soy estudiante de ingeniería de sistemas
+                        Me apasiona aprender cosas nuevas, de cualquier tema,
+                        me apasiona conocer personas ampliar mi circulo social.< br />
+                        <br />
+                        Soy entusiasta, y me esfuerzo por aprender tecnologías como
+                        Java, Javascrpit, HTML, CSS, React, Linux, herramientas
+                        de búsqueda de vulnerabilidades, la computación
+                        en la nube como AWS, Oracle cloud
+                        </p>
 
-                    
-                    <div className='cancion'>
-                        <button onClick={anterior} >
-                            <img 
-                                src="../anterior.png" 
-                                width={25}
-                                height={25}
-                                alt="former" />
-                        </button>
-                        <button onClick={reproducir}>
-                            <img 
-                                src={reproduciendo ? "../pause.png": "../play.png"} 
-                                height={35}
-                                width={35}
-                                alt="" />
-                        </button>
-                               
-                        <button onClick={cambioCancion}>
-                            <img 
-                                 src="../siguiente.png" 
-                                width={25}
-                                height={25}
-                                alt="next" />
-                        </button>
-                    </div>             
+                        
+                        <div className='cancion'>
+                            <button onClick={anterior} >
+                                <img 
+                                    src="../anterior.png" 
+                                    width={25}
+                                    height={25}
+                                    alt="former" />
+                            </button>
+                            <button onClick={reproducir}>
+                                <img 
+                                    src={reproduciendo ? "../pause.png": "../play.png"} 
+                                    height={35}
+                                    width={35}
+                                    alt="" />
+                            </button>
+                                
+                            <button onClick={cambioCancion}>
+                                <img 
+                                    src="../siguiente.png" 
+                                    width={25}
+                                    height={25}
+                                    alt="next" />
+                            </button>
+                        </div>             
+                    </div>    
                 </div>
             </div>
 
